769796017f9a6274ab9e57c0980b670c3b68ad5e
yhui8899 <58974214+yhui8899@users.noreply.github.com>
2019-12-18 09:31:19 +0800

    Add files via upload

---
+++ Mysql/monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+Uptime=`mysqladmin --defaults-extra-file=tmp.txt status | awk '{print $2}'`
+QPS() {
+ Questions=`mysqladmin --defaults-extra-file=tmp.txt status | awk '{print $6}'`
+ awk 'BEGIN{printf "%.2f\n",'$Questions'/'$Uptime'}'
+}
+#TPS
+TPS() {
+ rollback=`mysqladmin --defaults-extra-file=tmp.txt extended-status | awk '/\<Com_rollback\>/{print $4}'`
+ commit=`mysqladmin --defaults-extra-file=tmp.txt extended-status | awk '/\<Com_commit\>/{print $4}'`
+ awk 'BEGIN{printf "%.2f\n",'$(($rollback+$commit))'/'$Uptime'}'
+}
+$1
